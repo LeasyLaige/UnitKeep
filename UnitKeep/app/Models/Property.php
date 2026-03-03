43e5ad3b605bc708bf5b689df4d98345cb6ee74c
Ly93DX <alyssa.garcia@mymail.mapua.edu.ph>
2026-03-03 13:29:02 +0800

    Update Property.php

--- UnitKeep/app/Models/Property.php
+++ UnitKeep/app/Models/Property.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'unit_number',
+        'building_name',
+        'floor',
+        'monthly_rent',
+        'deposit_amount',
+        'status',
+        'description',
+    ];
+}
