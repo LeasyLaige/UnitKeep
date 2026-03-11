457aa968923092d0e7d99eb39e8fec3366633e64
LeasyLaige <lelouchiuvora243@gmail.com>
2026-03-12 01:57:43 +0800

    Add database seeding to the start script

--- UnitKeep/docker/start.sh
+++ UnitKeep/docker/start.sh
@@ -8,6 +8,7 @@ php artisan route:clear
 php artisan view:clear
 
 php artisan migrate --force
+php artisan db:seed --force
 php artisan storage:link || true
 php artisan config:cache
 php artisan route:cache
